583bbd435eff43af6c8d43f0c715386bc6326116
ALYTV <alitvinenkoo13@gmail.com>
2025-06-17 02:53:07 +0200

    upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ASCII-GEN
 Simple img2img ascii
+Based off [ASCII-Generator by vietnh1009](https://github.com/vietnh1009/ASCII-generator)
+
+## Run example:
+python3 img2img.py --input data/ballsackbird.webp --output data/output.jpg --language english --mode standard --background white --num_cols 200
+
+## Path to input
+"--input", type=str, default="data/input.jpg"
+## Path to output
+"--output", type=str, default="data/output.jpg"
+## Language (alphabets.py)
+"--language", type=str, default="english")
+## Mode
+"--mode", type=str, default="standard")
+## Background Color
+"--background", type=str, default="black", choices=["black", "white"]
+## Number of characters within width
+"--num_cols", type=int, default=300, help="number of character for output's width")
+
+### Requirements
+numpy
+pillow
+opencv-python
---
+++ alphabets.py
@@ -0,0 +1,37 @@
+GENERAL = {
+    "simple": "@%#*+=-:. ",
+    "complex": r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+}
+
+CHINESE = {
+    "standard": "龘䶑瀰幗獼鑭躙䵹觿䲔釅欄鐮䥯鶒獭鰽襽螻鰱蹦屭繩圇婹歜剛屧磕媿慪像僭堳噞呱棒偁呣塙唑浠唼刻凌咄亟拮俗参坒估这聿布允仫忖玗甴木亪女去凸五圹亐囗弌九人亏产斗丩艹刂彳丬了５丄三亻讠厂丆丨１二宀冖乛一丶、",
+}
+
+KOREAN = {
+    "standard": "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣ"
+}
+
+JAPANESE = {
+    "hiragana": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん",
+    "katakana": "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
+}
+
+ENGLISH = {
+    "standard": "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+}
+
+RUSSIAN = {
+    "standard": "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+}
+
+GERMAN = {
+    "standard": "AaÄäBbßCcDdEeFfGgHhIiJjKkLlMmNnOoÖöPpQqRrSsTtUuÜüVvWwXxYyZz"
+}
+
+FRENCH = {
+    "standard": "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzÆæŒœÇçÀàÂâÉéÈèÊêËëÎîÎïÔôÛûÙùŸÿ"
+}
+
+SPANISH = {
+    "standard": "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzÑñáéíóú¡¿"
+}
